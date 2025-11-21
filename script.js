5291c3d09c59e0cb8dce34fc67e88a2aa2efb629
sergokss <89041058+sergokss@users.noreply.github.com>
2025-11-21 23:08:25 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const button = document.getElementById('yesButton');
-
-button.addEventListener('click', () => {
-  // Если локальное фото
-  window.open('photo.jpg', '_blank');
-
-  // Если онлайн фото
-  // window.open('https://example.com/photo.jpg', '_blank');
-});
